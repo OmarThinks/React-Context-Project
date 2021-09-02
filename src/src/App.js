0ea198a130f547b6db94a1b108f9892c9bb58f44
Omar Magdy <contact.parmag@gmail.com>
2021-09-02 18:13:29 +0200

    navbar.js:add:component

--- src/src/App.js
+++ src/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-
-
-
-
-
 import React, { Component } from 'react';
 import Main from './components/main';
+import NavBar from './components/navbar';
+
+
+
 class App extends Component {
   state = {  }
   render() { 
       return(
         <div>
+          <NavBar></NavBar>
           <Main></Main>
         </div>        
      );
---
+++ src/src/components/navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>My Cart</li>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default NavBar;
+
