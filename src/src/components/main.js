f3565d2556956af756c138924868da588edb8347
Omar Magdy <contact.parmag@gmail.com>
2021-09-02 19:27:49 +0200

    main.js:add:items

--- src/src/components/main.js
+++ src/src/components/main.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class Main extends Component {
     state = {  }
     render() { 
-        return ( <div>This is main</div> );
+        return ( 
+        <div>
+            <ul>
+                <li> iPhone </li>
+                <li> Labtop </li>
+                <li> Android Mobile </li>
+            </ul>
+        </div> );
     }
 }
  
