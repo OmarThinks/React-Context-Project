8b7e8d63ee9537a63327bd9f41bd3b35aa63be73
Omar Magdy <contact.parmag@gmail.com>
2021-09-27 23:23:52 +0200

    added useState

--- 2-hooks-app/src/components/ProductsList.js
+++ 2-hooks-app/src/components/ProductsList.js
@@ -1,9 +1,21 @@
+import React,{useState} from 'react';
+
+
 const ProductsList = () => {
+    
+    const [products,setProducts]= useState(
+        [
+            {name:"CPU", id: 1},
+            {name:"Labtop", id: 2},
+            {name:"Mobile", id: 3},
+        ]
+    );
+
     return ( 
     <div className="prodcuts-list">
         <ul>
-        <li>CPU</li>
-        <li>Labtop</li>
+            <li>CPU</li>
+            <li>Labtop</li>
         </ul>
     </div> );
 }
