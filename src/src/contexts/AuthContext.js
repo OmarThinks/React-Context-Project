6861438b04787aa676da848773c8714836f16001
Omar Magdy <contact.parmag@gmail.com>
2021-09-27 18:18:11 +0200

    renamed to AuthContext

---
+++ src/src/contexts/AuthContext.js
@@ -0,0 +1,23 @@
+import {createContext, Component} from 'react';
+
+export const AuthContext = createContext();
+
+class AuthContextProvider extends Component {
+    state = { 
+        isAuthenticated:false,
+    };
+
+    toggleAuth=()=>{
+        this.setState({isLightTheme:!this.state.isAuthenticated});
+    }
+
+    render() { 
+        return ( 
+        <AuthContext.Provider value={
+            {...this.state, toggleAuth:this.toggleAuth}}>
+            {this.props.children}
+        </AuthContext.Provider> );
+    }
+}
+ 
+export default AuthContextProvider;
