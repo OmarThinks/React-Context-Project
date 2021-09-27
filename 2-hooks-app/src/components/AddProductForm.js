37d2618132a70ca68743af5d225d1260a93303ef
Omar Magdy <contact.parmag@gmail.com>
2021-09-28 00:15:36 +0200

    created the titile state

--- 2-hooks-app/src/components/AddProductForm.js
+++ 2-hooks-app/src/components/AddProductForm.js
@@ -3,10 +3,16 @@ import React,{useState} from 'react';
 
 const AddProductForm = () => {
     
+
+    const [title,setTitle] = useState("Hey");
+
+
     return ( 
         <form>
             <label>Product Name:</label>
-            <input type="text" name="name"/>
+            <input type="text" name="name" 
+            value={title}
+            required/>
             <input type="submit" value="Create a Product"/>
         </form>
      );
