af7498073b0b91d15d9e572b0d595e1366df3368
Omar Magdy <contact.parmag@gmail.com>
2021-09-27 14:03:45 +0200

    main.js now uses the style

--- src/src/components/main.js
+++ src/src/components/main.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 
 
 
 class Main extends Component {
-
+    static contextType = ThemeContext;
     render() { 
+        console.log(this.context);
+        const {isLightTheme, light, dark}=this.context;
+        const theme = isLightTheme ? light : dark;
+
         return ( 
-        <main>
+        <main style={{background:theme.bg, color: theme.syntax}}>
             <ul>
                 <li>First element</li>
                 <li>Second element</li>
