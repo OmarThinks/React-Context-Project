62ad2182b4fbaaad75b6fac5a85302dc66237235
Omar Magdy <contact.parmag@gmail.com>
2021-09-27 22:57:28 +0200

    created empty hooks app

---
+++ 2-hooks-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Component } from 'react';
+
+
+class App extends Component {
+  state = {  }
+  render() { 
+      return(
+        <div id="root_div">
+
+        </div>        
+     );
+  }
+}
+
+export default App;
