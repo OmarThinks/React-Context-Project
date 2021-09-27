eb554370c127c1e479c2069b29e7c07fd13df31e
Omar Magdy <contact.parmag@gmail.com>
2021-09-27 13:31:22 +0200

    changed the colors according to context

--- src/src/components/navbar.js
+++ src/src/components/navbar.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class NavBar extends Component {
-
-    render() { 
+    static contextType = ThemeContext;
+    /*
+        This climbs up the component tree, to find the first time 
+        there is a provider for this context
+    */
+    render() {
+        console.log(this.context);
+        const {isLightTheme, light, dark}=this.context;
+        const theme = isLightTheme ? light : dark;
         return ( 
-          <navbar>
+          <nav style={{background:theme.bg, color: theme.syntax}}>
               <ul>
                 <li>Home</li>
                 <li>Products</li>
                 <li>Login</li>
                 <li>Logout</li>
               </ul>
-          </navbar>
+          </nav>
          );
     }
 }
