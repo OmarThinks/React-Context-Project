ac6c8135dc831557dd398fb8dfeb6731903f2e8c
Omar Magdy <contact.parmag@gmail.com>
2021-09-28 20:12:13 +0200

    AuthContext Converted to sfc

--- 1-context-app/src/contexts/AuthContext.js
+++ 1-context-app/src/contexts/AuthContext.js
@@ -1,7 +1,5 @@
-import {createContext, Component} from 'react';
 
-export const AuthContext = createContext();
-
+/*
 class AuthContextProvider extends Component {
     state = { 
         isAuthenticated:false,
@@ -19,5 +17,27 @@ class AuthContextProvider extends Component {
         </AuthContext.Provider> );
     }
 }
+*/
+
+
+import {createContext, useState} from 'react';
+export const AuthContext = createContext();
+
+const AuthContextProvider = (props) => {
+    
+    const [isAuthenticatedState, setIsAuthenticatedState] = useState({isAuthenticated:false});
+
+    const toggleAuth=()=>{
+        setIsAuthenticatedState({isAuthenticated:!isAuthenticatedState.isAuthenticated});
+    }
+
+    return ( 
+    <AuthContext.Provider value={{...isAuthenticatedState, toggleAuth}}>
+        {props.children}
+    </AuthContext.Provider> );
+}
  
 export default AuthContextProvider;
+
+
+
