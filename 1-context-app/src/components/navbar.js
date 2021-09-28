bb471a8f04193da48743496747eb65718621f349
Omar Magdy <contact.parmag@gmail.com>
2021-09-28 18:21:25 +0200

    useContext inside NavBar

--- 1-context-app/src/components/navbar.js
+++ 1-context-app/src/components/navbar.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
-
+/*
 class NavBar extends Component {
     static contextType = ThemeContext;
-    /*
-        This climbs up the component tree, to find the first time 
-        there is a provider for this context
-    */
+    //This climbs up the component tree, to find the first time 
+    //there is a provider for this context
+    
     render() {
-        console.log(this.context);
-        const {isLightTheme, light, dark}=this.context;
-        const theme = isLightTheme ? light : dark;
+
         return ( 
-          <nav style={{background:theme.ui, color: theme.syntax}}>
-              <ul>
-                <li>Home</li>
-                <li>Products</li>
-                <li>Login</li>
-                <li>Logout</li>
-              </ul>
-          </nav>
+          
          );
     }
 }
+*/
+
+const NavBar = () => {
+    const context =useContext(ThemeContext);
+    console.log(context);
+    const {isLightTheme, light, dark}=context;
+    const theme = isLightTheme ? light : dark;
+    return ( <nav style={{background:theme.ui, color: theme.syntax}}>
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>Login</li>
+          <li>Logout</li>
+        </ul>
+    </nav> );
+}
  
 export default NavBar;
+
 
